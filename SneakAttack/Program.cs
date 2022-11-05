7f324b2afc108d670473024e39906c91fbbd30d4
Solomon <soloon.s@x-pressfeeders.com>
2022-11-05 11:55:31 +0530

    Iteration #1

--- SneakAttack/Program.cs
+++ SneakAttack/Program.cs
@@ -6,23 +6,23 @@ internal class Program
 {
     private static void Main(string[] args)
     {
-        
+        //Iteration 1
         Console.WriteLine("No of players: ");
         var n = Convert.ToInt32(Console.ReadLine());
 
         List<Player> playerList = new List<Player>();
         bool killerCreated = false;
 
-        Array values = Enum.GetValues(typeof(Roles));
         Random rnd = new Random();
 
         for (var i = 0; i < n; i++)
         {
-            Roles randomRole = RandomRole(killerCreated, rnd, values);
+            Roles randomRole = RandomRole(killerCreated, rnd);
             if(randomRole == Roles.Killer)  killerCreated = true;
 
             var role = new Role(randomRole);
             var player = new Player(role);
+
             playerList.Add(player);
 
             Console.WriteLine(player.PlayerRole.RoleName.ToString());
@@ -31,8 +31,9 @@ internal class Program
 
     }
 
-    private static Roles RandomRole(bool isKillerCreated, Random random, Array values)
+    private static Roles RandomRole(bool isKillerCreated, Random random)
     {
+        Array values = Enum.GetValues(typeof(Roles));
         if (isKillerCreated)
         {
             Roles randomRole = (Roles)values.GetValue(random.Next(values.Length));
